6950dbeba0416cb223840afe2cab0fb9062b8020
Todd Wolfson <todd@twolfson.com>
2016-10-20 02:30:54 -0700

    Got our environment passing working. Finally ready to get started on our proof of concept

--- gemini.js
+++ gemini.js
@@ -1,3 +1,10 @@
+// Load in our dependencies
+var assert = require('assert');
+
+// Verify we have an ENV environment variable set
+var env = process.env.ENV;
+assert(env, '`ENV` environment variable wasn\'t set. Please set it to `normal` or `alt`');
+
 // Define our resize helpers
 // DEV: Despite using a very tall height, we only capture until the body stops
 // DEV: These originally come from a `utils/gemini.js` file
@@ -15,7 +22,7 @@ var geminiUtils = {
 
 // Define our visual tests
 gemini.suite('root', function (suite) {
-  suite.setUrl('/')
+  suite.setUrl('/?env=' + encodeURIComponent(env))
     .setCaptureElements('body')
     .capture('default-large', geminiUtils.resizeLarge)
     .capture('default-medium', geminiUtils.resizeMedium)
