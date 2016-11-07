7f921e8eb4de648d4cdf4915e64cb7219cba1ff8
Todd Wolfson <todd@twolfson.com>
2016-11-07 05:01:57 -0800

    Shifting around dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -22,6 +22,7 @@ module.exports = function (config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'test/**/*.js': ['browserify']
     },
 
     // configure browserify
