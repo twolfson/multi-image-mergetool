10fe8b16239f63a5df0525864055b69b77d096ed
Todd Wolfson <todd@twolfson.com>
2016-11-15 04:58:51 -0800

    Bumping up setTimeout out of frustration

--- test/browser/application-test.js
+++ test/browser/application-test.js
@@ -165,7 +165,7 @@ describe('When we click/drag on a diff image', function () {
       var imageSetCollapseEl = imageSetEl.querySelector('.image-set__collapse');
       $(imageSetTitleEl).click();
       expect([].slice.call(imageSetCollapseEl.classList)).to.not.include('in');
-      setTimeout(done, 0);
+      setTimeout(done, 100);
     });
 
     it('makes the overlay no longer visible', function () {
