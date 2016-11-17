cf64a1e328a66c76de5f77d25dd7872622b59c59
Todd Wolfson <todd@twolfson.com>
2016-11-17 01:58:56 -0800

    Added sinon for stubbing updateRef action

--- test/server/update-image-set-filepath-show.js
+++ test/server/update-image-set-filepath-show.js
@@ -4,11 +4,12 @@ var expect = require('chai').expect;
 var ImageSet = require('../../server/image-set');
 var httpUtils = require('./utils/http');
 var serverUtils = require('./utils/server');
+var sinonUtils = require('../utils/sinon');
 
 // Start our tests
 describe('A request to POST /update-image-set/:filepath', function () {
   describe('for an existent filepath', function () {
-    // Run a server with our temporary file
+    // Run a server, stub out updating files, and make our request
     var srcFilepath = __dirname + '/../test-files/checkerboard.png';
     serverUtils.run(ImageSet.generateSets([
       'mock-image-1/current.png'
@@ -17,6 +18,12 @@ describe('A request to POST /update-image-set/:filepath', function () {
     ], {
       diffImages: ['mock-image-1/diff.png']
     }));
+    sinonUtils.stub(ImageSet.prototype, 'updateRef', function updateRefStub (refBuff, cb)  {
+      // Call our callback in a second (prevent zalgo) with mocked result
+      process.nextTick(function handleNextTick () {
+        cb(null, 'mocked');
+      });
+    });
     before(function makeRequest (done) {
       var base64Contents = 'data:image/png,base64;' + fs.readFileSync(srcFilepath).toString('base64');
       httpUtils._save({
@@ -29,7 +36,12 @@ describe('A request to POST /update-image-set/:filepath', function () {
     });
 
     it('updated reference image with new contents', function () {
-      expect(JSON.parse(this.body)).to.deep.equal({imagesEqual: 'mocked'});
+      var updateRefStub = ImageSet.prototype.updateRef;
+      expect(updateRefStub.callCount).to.equal(1);
+      var expectedContents = fs.readFileSync(srcFilepath);
+      // expect(updateRefStub.args[0][0]).to.deep.equal(expectedContents);
+      console.log(updateRefStub.args[0][0]);
+      console.log(expectedContents);
     });
 
     it('replies with imagesEqual info', function () {
