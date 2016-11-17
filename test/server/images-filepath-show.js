58466c162c9e836b09438fd5d07ef5f5acbac4b0
Todd Wolfson <todd@twolfson.com>
2016-11-17 01:00:38 -0800

    Starting on tests for /images/:filepath

---
+++ test/server/images-filepath-show.js
@@ -0,0 +1,39 @@
+// Load in our dependencies
+var fs = require('fs');
+var expect = require('chai').expect;
+var ImageSet = require('../../server/image-set');
+var httpUtils = require('./utils/http');
+var serverUtils = require('./utils/server');
+
+// Start our tests
+describe('An request to GET /images/:filepath', function () {
+  describe('for an existent absolute filepath', function () {
+    // Create our server and make our request
+    var existentFilepath = __dirname + '/../test-files/dot.png';
+    console.log(__dirname + '/../test-files/dot.png');
+    serverUtils.run(ImageSet.generateSets([
+      existentFilepath
+    ], [
+      existentFilepath
+    ], {
+      diffImages: [existentFilepath]
+    }));
+    httpUtils.save({
+      method: 'GET', url: serverUtils.getUrl('/images/' + encodeURIComponent(existentFilepath)),
+      expectedStatusCode: 200
+    });
+
+    it('receives our image', function () {
+      var expectedContents = fs.readFileSync(existentFilepath);
+      expect(this.body).to.deep.equal(expectedContents);
+    });
+  });
+
+  describe.skip('for an existent relative filepath', function () {
+
+  });
+
+  describe.skip('for a non-existent filepath', function () {
+
+  });
+});
