b0629f9b02c3989ddf6aa119b949919086d3f690
Todd Wolfson <todd@twolfson.com>
2016-11-17 23:06:02 -0800

    Moved to nested setup for clarity

--- test/server/cli.js
+++ test/server/cli.js
@@ -26,30 +26,32 @@ var childUtils = {
 // Start our tests
 // DEV: These are sanity checks for parse wrapper
 var multiImageMergetoolFilepath = __dirname + '/../../bin/multi-image-mergetool';
-describe('A CLI invocation with matching images', function () {
-  childUtils.spawn('node', [
-    multiImageMergetoolFilepath,
-    '--current-images', __dirname + '/../test-files/dot.png',
-    '--ref-images', __dirname + '/../test-files/dot.png'
-  ]);
+describe('A CLI invocation', function () {
+  describe('with matching images', function () {
+    childUtils.spawn('node', [
+      multiImageMergetoolFilepath,
+      '--current-images', __dirname + '/../test-files/dot.png',
+      '--ref-images', __dirname + '/../test-files/dot.png'
+    ]);
 
-  it('exits cleanly', function () {
-    expect(this.err).to.equal(null);
-    expect(this.stdout).to.match(/✓.+test-files\/dot\.png/);
-    expect(this.stdout).to.contain('Images matched: 1 of 1');
+    it('exits cleanly', function () {
+      expect(this.err).to.equal(null);
+      expect(this.stdout).to.match(/✓.+test-files\/dot\.png/);
+      expect(this.stdout).to.contain('Images matched: 1 of 1');
+    });
   });
-});
 
-describe('A CLI invocation with uneven image counts', function () {
-  childUtils.spawn('node', [
-    multiImageMergetoolFilepath,
-    '--current-images', __dirname + '/../test-files/dot.png', __dirname + '/../test-files/dot.png',
-    '--ref-images', __dirname + '/../test-files/dot.png'
-  ]);
+  describe('with uneven image counts', function () {
+    childUtils.spawn('node', [
+      multiImageMergetoolFilepath,
+      '--current-images', __dirname + '/../test-files/dot.png', __dirname + '/../test-files/dot.png',
+      '--ref-images', __dirname + '/../test-files/dot.png'
+    ]);
 
-  it('exits uncleanly', function () {
-    expect(this.err).to.have.property('status', 1);
-    expect(this.err.stderr).to.contain(
-      '2 current images and 1 reference images were received. We expect these numbers to line up');
+    it('exits uncleanly', function () {
+      expect(this.err).to.have.property('status', 1);
+      expect(this.err.stderr).to.contain(
+        '2 current images and 1 reference images were received. We expect these numbers to line up');
+    });
   });
 });
