a49ab5913b012f584b0686d4ec2ca38aed70a7a9
Todd Wolfson <todd@twolfson.com>
2016-11-17 23:19:04 -0800

    Starting on in-process CLI testing

--- test/server/cli.js
+++ test/server/cli.js
@@ -1,16 +1,39 @@
 // Load in our dependencies
 var expect = require('chai').expect;
 var childUtils = require('./utils/child');
+var sinonUtils = require('../utils/sinon');
+var cliParse = require('../../server/cli').parse;
+var logger = require('../../server/logger');
 var multiImageMergetoolFilepath = __dirname + '/../../bin/multi-image-mergetool';
+var checkerboardFilepath = __dirname + '/../test-files/checkerboard.png';
+var dotFilepath = __dirname + '/../test-files/dot.png';
 
 // Start our tests
+describe('An in-process CLI invocation', function () {
+  describe('with matching images', function () {
+    // sinonUtils.stub(logger, 'log'); sinonUtils.stub(logger, 'info');
+    sinonUtils.stub(process, 'exit');
+    cliParse(['node',
+      multiImageMergetoolFilepath,
+      '--current-images', dotFilepath, checkerboardFilepath,
+      '--ref-images', dotFilepath, checkerboardFilepath
+    ]);
+
+    it('exits cleanly', function () {
+      var processExitStub = process.exit;
+      expect(processExitStub.callCount).to.equal(1);
+      expect(processExitStub.args[0]).to.deep.equal([0]);
+    });
+  });
+});
+
 // DEV: These are sanity checks for parse wrapper
 describe('A CLI invocation', function () {
   describe('with matching images', function () {
     childUtils.spawn('node', [
       multiImageMergetoolFilepath,
-      '--current-images', __dirname + '/../test-files/dot.png',
-      '--ref-images', __dirname + '/../test-files/dot.png'
+      '--current-images', dotFilepath,
+      '--ref-images', dotFilepath
     ]);
 
     it('exits cleanly', function () {
@@ -23,8 +46,8 @@ describe('A CLI invocation', function () {
   describe('with uneven image counts', function () {
     childUtils.spawn('node', [
       multiImageMergetoolFilepath,
-      '--current-images', __dirname + '/../test-files/dot.png', __dirname + '/../test-files/dot.png',
-      '--ref-images', __dirname + '/../test-files/dot.png'
+      '--current-images', dotFilepath, dotFilepath,
+      '--ref-images', dotFilepath
     ]);
 
     it('exits uncleanly', function () {
