68c2306bbe1bcff686a4bc520741332a7e0caf78
Todd Wolfson <todd@twolfson.com>
2016-11-19 00:32:47 -0800

    Starting on update similar images tests

--- test/browser/application-update-similar-images.js
+++ test/browser/application-update-similar-images.js
@@ -9,6 +9,8 @@ var applicationUtils = require('./utils/application');
 var mouseUtils = require('./utils/mouse');
 var updateImageSetFilepathEqualResponse = fs.readFileSync(
   __dirname + '/../test-files/http-responses/update-image-set-filepath-equal.json', 'utf8');
+var updateImageSetFilepathNotEqualResponse = fs.readFileSync(
+  __dirname + '/../test-files/http-responses/update-image-set-filepath-not-equal.json', 'utf8');
 
 // Define reused actions in tests
 // TODO: Consolidate reused actions in tests (currently not sober enough to consolidate them wisely)
@@ -35,30 +37,45 @@ function approveAllXHRUpdates() {
     url: /\/update-image-set\/[^\/]+/,
     statusCode: 200,
     headers: {'Content-Type': 'application/json'},
-    body: updateImageSetFilepathEqualResponse // {imagesEqual: true}
+    // jscs:disable maximumLineLength
+    // Set via: echo -n '{"imagesEqual":false}' > test/test-files/http-responses/update-image-set-filepath-not-equal.json
+    // jscs:enable maximumLineLength
+    body: updateImageSetFilepathNotEqualResponse // {imagesEqual: false}
   }]);
 }
-function clickAcceptSimilarImages() {
+function disapproveAllXHRUpdates() {
+  sinonUtils.mockXHR([{
+    method: 'POST',
+    url: /\/update-image-set\/[^\/]+/,
+    statusCode: 200,
+    headers: {'Content-Type': 'application/json'},
+    // jscs:disable maximumLineLength
+    // Set via: echo -n '{"imagesEqual":false}' > test/test-files/http-responses/update-image-set-filepath-not-equal.json
+    // jscs:enable maximumLineLength
+    body: updateImageSetFilepathNotEqualResponse // {imagesEqual: false}
+  }]);
+}
+function clickUpdateSimilarImages() {
   var buttonEl = this.containerEl.querySelector(
-    '[data-image-set="mock-img-not-equal"] button[data-action="accept-similar-images"]');
+    '[data-image-set="mock-img-not-equal"] button[data-action="update-similar-images"]');
   assert(buttonEl);
   $(buttonEl).click();
 }
 
 // Start our tests
 describe('An application with similarly failing images', function () {
-  describe('when accepting some similarly failing images', function () {
+  describe.only('when updating some similarly failing images partially', function () {
     applicationUtils.init(applicationUtils.IMAGE_SETS.MULTIPLE_NOT_EQUAL);
     before(overlayDiffImg);
     before(clickFindSimilarImages);
-    approveAllXHRUpdates();
+    disapproveAllXHRUpdates();
     before(function deselectSimilarImageSets () {
       var currentSimilarImageSetEl = this.containerEl.querySelector('[data-similar-image-set="mock-img-not-equal"]');
       var saveUpdateEl = currentSimilarImageSetEl.querySelector('[name=save_update]');
       saveUpdateEl.checked = false;
     });
-    before(clickAcceptSimilarImages);
-    applicationUtils.screenshot('accept-some-similar-images');
+    before(clickUpdateSimilarImages);
+    applicationUtils.screenshot('update-similar-images-partially');
 
     it('updates selected images in full in its image set', function () {
       // Verify image set status updated
@@ -89,19 +106,25 @@ describe('An application with similarly failing images', function () {
       // DEV: XHR assertions are done in previous `it`
       //   Mostly via length check + url check
     });
-
-    it.skip('removes results for similar images', function () {
-      // Placeholder content
-    });
   });
 
-  describe('when accepting all similarly failing images', function () {
+  describe('when updating some similarly failing images fully', function () {
     applicationUtils.init(applicationUtils.IMAGE_SETS.MULTIPLE_NOT_EQUAL);
-    before(overlayDiffImg);
+    before(function overlayDiffImg (done) {
+      // DEV: We use an expanded image set so we can click/drag
+      var diffImg = this.containerEl.querySelector('[data-image-set="mock-img-not-equal"] img[data-compare-type=diff]');
+      var diffImgBounds = diffImg.getBoundingClientRect();
+      mouseUtils.moveMouse({
+        targetEl: diffImg,
+        startCoords: {x: diffImgBounds.left, y: diffImgBounds.top},
+        endCoords: {x: diffImgBounds.left + 15, y: diffImgBounds.top + 15},
+        duration: 100 // ms
+      }, done);
+    });
     before(clickFindSimilarImages);
     approveAllXHRUpdates();
-    before(clickAcceptSimilarImages);
-    applicationUtils.screenshot('accept-all-similar-images');
+    before(clickUpdateSimilarImages);
+    applicationUtils.screenshot('update-similar-images-fully');
 
     it.skip('collapses current image set', function () {
       // Placeholder content
