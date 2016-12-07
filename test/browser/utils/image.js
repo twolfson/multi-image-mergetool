4af4dae2af24834d996058b38b54dba56a0536e0
Todd Wolfson <todd@twolfson.com>
2016-12-07 00:07:19 -0800

    Starting on image generating utilities

---
+++ test/browser/utils/image.js
@@ -0,0 +1,47 @@
+// Define image generators
+exports.getDiagonalBase64 = function () {
+  // Create an ndarray with our pattern
+  // +---------------+
+  // |xxxxx          |
+  // |xxxxx          |
+  // |xxxxx          |
+  // |xxxxx          |
+  // |xxxxx          |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |          xxxxx|
+  // |          xxxxx|
+  // |          xxxxx|
+  // |          xxxxx|
+  // |          xxxxx|
+  // +---------------+
+
+  // Create a canvas and write in the data
+  // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
+  // https://github.com/scijs/save-pixels/blob/f7bf8c831927e3ade93ffe499b04f4013d641b93/save-pixels.js#L126-L136
+
+  // Export the base64 result
+};
+
+exports.getDiagonalBase64 = function () {
+  // +---------------+
+  // |               |
+  // |               |
+  // |               |
+  // |               |
+  // |               |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |     xxxxx     |
+  // |               |
+  // |               |
+  // |               |
+  // |               |
+  // |               |
+  // +---------------+
+};
