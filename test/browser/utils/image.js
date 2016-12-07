f991c0c3026823eab0d08077aa3a8ec52b2fd232
Todd Wolfson <todd@twolfson.com>
2016-12-07 00:15:05 -0800

    Completed first image ndarray generator

--- test/browser/utils/image.js
+++ test/browser/utils/image.js
@@ -1,6 +1,11 @@
+// Load in our dependencies
+var ndarrayFill = require('ndarray-fill');
+var zeros = require('zeros');
+
 // Define image generators
-exports.getDiagonalBase64 = function () {
+exports._getDiagonalNdarray = function () {
   // Create an ndarray with our pattern
+  // https://github.com/twolfson/gif-encoder/blob/0.6.0/test/gif-encoder_test.js#L115-L132
   // +---------------+
   // |xxxxx          |
   // |xxxxx          |
@@ -18,6 +23,29 @@ exports.getDiagonalBase64 = function () {
   // |          xxxxx|
   // |          xxxxx|
   // +---------------+
+  var imageNdarray = zeros([15, 15, 4]);
+  ndarrayFill(imageNdarray, function fillImageNdarray (x, y, rgbaIndex) {
+    // If this is the alpha channel, always return it as full
+    if (rgbaIndex === 3) {
+      return 0xFF;
+    }
+
+    // Otherwise, if we are on our black dot, then draw it
+    if ((0  < x && 0  < y && x < 5  && y < 5) ||
+        (5  < x && 5  < y && x < 10 && y < 10) ||
+        (10 < x && 10 < y && x < 15 && y < 15)) {
+      // Generate black dot (00 00 00)
+      return 0x00;
+    // Otherwise, draw white (FF FF FF)
+    } else {
+      return 0xFF;
+    }
+  });
+};
+
+exports.getDiagonalBase64 = function () {
+  // Grab our ndarray data
+  var imageNdArray = exports._getDiagonalNdarray();
 
   // Create a canvas and write in the data
   // https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
