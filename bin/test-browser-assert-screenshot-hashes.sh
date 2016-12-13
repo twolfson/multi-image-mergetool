f5d57d55e7b873561bb97370b769d16a6bfde899
Todd Wolfson <todd@twolfson.com>
2016-12-13 02:21:44 -0800

    Completed filepath resolution but realizing sha256sum has all these features

--- bin/test-browser-assert-screenshot-hashes.sh
+++ bin/test-browser-assert-screenshot-hashes.sh
@@ -11,6 +11,14 @@ shopt -s globstar
 rm test/browser/expected-screenshots/**/*.png.sha256 &> /dev/null || true
 
 # Compute new hashes
-for filepath in test/browser/actual-screenshots/**/*.png; do
-  echo "$filepath"
+for src_filepath in test/browser/actual-screenshots/**/*.png; do
+  # Resolve our new filepath
+  # test/browser/actual-screenshots/accept-all-similar-images.png
+  #   -> test/browser/expected-screenshots/accept-all-similar-images.png
+  #   -> test/browser/expected-screenshots/accept-all-similar-images.png.sha256
+  target_filepath="$(echo "$src_filepath" | sed "s/actual-screenshots/expected-screenshots/")"
+  target_filepath="$target_filepath.sha256"
+
+  # Output its new hash
+  sha256sum "$src_filepath"
 done
