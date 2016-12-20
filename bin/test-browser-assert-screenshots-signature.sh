cb2032f6b7e28424b2ca4318d29f9a96c1a5222d
Todd Wolfson <todd@twolfson.com>
2016-12-20 00:07:27 -0800

    Renamed scripts to represent new tests

--- bin/test-browser-assert-screenshots-signature.sh
+++ bin/test-browser-assert-screenshots-signature.sh
@@ -2,22 +2,22 @@
 # Exit on first error
 set -e
 
-# Update our sha256 info for both folders
-# DEV: In CI, we don't have expected screenshots so opt-out of a SHA
+# Update our image signatures for both folders
+# DEV: In CI, we don't have expected screenshots so opt-out of updates
 if test "$(echo test/browser/expected-screenshots**/*.png)" != ""; then
   # DEV: Run our updates in parallel for performance
-  bin/test-browser-update-screenshots-hash.sh test/browser/actual-screenshots &
-  bin/test-browser-update-screenshots-hash.sh test/browser/expected-screenshots &
+  bin/test-browser-update-screenshots-signature.sh test/browser/actual-screenshots &
+  bin/test-browser-update-screenshots-signature.sh test/browser/expected-screenshots &
 else
-  bin/test-browser-update-screenshots-hash.sh test/browser/actual-screenshots &
+  bin/test-browser-update-screenshots-signature.sh test/browser/actual-screenshots &
 fi
 wait
 
 # Compare our screenshot contents
 set +e
 diff --unified \
-  test/browser/expected-screenshots/contents.sha256 \
-  test/browser/actual-screenshots/contents.sha256
+  test/browser/expected-screenshots/contents.sig \
+  test/browser/actual-screenshots/contents.sig
 exit_code="$?"
 set -e
 
