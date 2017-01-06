34bd91cc21a6b85a4544132430abe1bf3abbb688
Todd Wolfson <todd@twolfson.com>
2017-01-05 22:36:13 -0800

    Added browser-dist copying

--- bin/generate-demo.sh
+++ bin/generate-demo.sh
@@ -6,15 +6,22 @@ set -e
 node bin/_gather-demo-screenshots.js
 
 # Generate diffs for our images
+# DEV: We ignore failure as it's expected
+set +e
 node bin/multi-image-mergetool \
     --assert \
     --current-images demo/current/*.png \
     --ref-images $(echo demo/current/*.png | sed "s/current/ref/g") \
     --diff-images $(echo demo/current/*.png | sed "s/current/diff/g")
+set -e
 
 # Update our image signatures
 # TODO: Add signature comparison to test suite -- likely via Travis CI (generate demo, verify no git changes)
 bin/test-browser-update-screenshots-signature.sh demo
+
+# Compile and copy our latest assets
+npm run build
+cp -r browser-dist demo/browser-dist
 
 # Generate our demo page
 ./node_modules/.bin/jade server/views/index.jade --obj demo/index.json --out demo
