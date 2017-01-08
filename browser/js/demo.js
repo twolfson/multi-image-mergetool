31ae7a790a681fdcf5eea212a71816f49804d1b8
Todd Wolfson <todd@twolfson.com>
2017-01-07 16:54:54 -0800

    Moved demo.js to testable setup

--- browser/js/demo.js
+++ browser/js/demo.js
@@ -4,20 +4,25 @@ var ImageSet = require('./image-set');
 var sinonUtils = require('../../test/utils/sinon');
 var xhrResponses = require('../../test/test-files/http-responses/xhr');
 
-// Define Mocha hook mocks
-global.before = function (fn) { fn(); };
-global.after = function (fn) { /* Don't run after functions */ };
+// Define our demo bindings
+window.runDemo = exports.runDemo = function (options) {
+  // If we are running live, overwrite Mocha's contents
+  if (options && options.overwriteMocha) {
+    global.before = function (fn) { fn(); };
+    global.after = function (fn) { /* Don't run after functions */ };
+  }
 
-// Start a Sinon server that approves all images
-sinonUtils.mockXHR([xhrResponses.UPDATE_IMAGE_SET_APPROVE]);
+  // Start a Sinon server that approves all images
+  sinonUtils.mockXHR([xhrResponses.UPDATE_IMAGE_SET_APPROVE]);
 
-// Mock over cachebustImg to always swap images to `current` variant
-sinonUtils.stub(ImageSet, 'cachebustImg', function cachebustImgstub (imgEl) {
-  // images/ref%2Froot.large.png -> images/current%2Froot.large.png
-  var originalSrc = imgEl.getAttribute('src');
-  var newSrc = originalSrc.replace(/images\/(ref|diff)/, 'images/current');
-  imgEl.setAttribute('src', newSrc);
-});
+  // Mock over cachebustImg to always swap images to `current` variant
+  sinonUtils.stub(ImageSet, 'cachebustImg', function cachebustImgstub (imgEl) {
+    // images/ref%2Froot.large.png -> images/current%2Froot.large.png
+    var originalSrc = imgEl.getAttribute('src');
+    var newSrc = originalSrc.replace(/images\/(ref|diff)/, 'images/current');
+    imgEl.setAttribute('src', newSrc);
+  });
+};
 
 // Load in normal script
 void require('./index.js');
