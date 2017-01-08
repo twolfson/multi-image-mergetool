cea387f5c73cc27046629c1e8404885bb1998f8b
Todd Wolfson <todd@twolfson.com>
2017-01-07 17:28:03 -0800

    Moved to broken out bindings for SimilarImageResults for easier mocking

--- browser/js/similar-image-results.js
+++ browser/js/similar-image-results.js
@@ -26,65 +26,65 @@ var SimilarImageResults = BaseComponent.extend({
   }
 });
 
+// Define our class level helpers
+// DEV: We don't inline these to make them mockable for demos
+SimilarImageResults.findSelectedSimilarImageSets = function (evt) {
+  // Find our image set container
+  var btnEl = evt.target;
+  var $imageSet = $(btnEl).closest('[data-image-set]');
+  assert.strictEqual($imageSet.length, 1);
+
+  // Find our similar image sets
+  // TODO: Use direct references instead of query selector
+  var $similarImageSets = $imageSet.find('[data-similar-image-set]');
+
+  // Filter our similar image sets to selected ones
+  return $similarImageSets.filter(function updateImageSet (i, similarImageSetEl) {
+    return similarImageSetEl.querySelector('[name=save_update]').checked;
+  });
+};
+SimilarImageResults.acceptSimilarImageSet = function (similarImageSetEl) {
+  // Move back to jQuery collection
+  var $similarImageSet = $(similarImageSetEl);
+  var similarImageSetId = $similarImageSet.data('similar-image-set');
+
+  // Find our original current image
+  var $originalCurrentImg = $similarImageSet.find('.original-current');
+  assert.strictEqual($originalCurrentImg.length, 1);
+  var originalCurrentImgBase64 = utils.getBase64Content($originalCurrentImg[0]);
+
+  // Run accept function
+  // TODO: Remove results when all loaded
+  var imageSet = GlobalState.fetchImageSetById(similarImageSetId);
+  imageSet.acceptChanges(originalCurrentImgBase64);
+};
+SimilarImageResults.updateSimilarImageSet = function (similarImageSetEl) {
+  // Move back to jQuery collection
+  var $similarImageSet = $(similarImageSetEl);
+  var similarImageSetId = $similarImageSet.data('similar-image-set');
+
+  // Extract updated base64 content
+  var $updatedRefCanvas = $similarImageSet.find('.updated-ref');
+  assert.strictEqual($updatedRefCanvas.length, 1);
+  var base64Data = $updatedRefCanvas[0].toDataURL('image/png');
+
+  // Run update function
+  // TODO: Remove results when all loaded
+  var imageSet = GlobalState.fetchImageSetById(similarImageSetId);
+  imageSet.acceptChanges(base64Data);
+};
+
 // Define our bindings
 SimilarImageResults.bindOnce = function () {
-  function findSelectedSimilarImageSets(evt) {
-    // Find our image set container
-    var btnEl = evt.target;
-    var $imageSet = $(btnEl).closest('[data-image-set]');
-    assert.strictEqual($imageSet.length, 1);
-
-    // Find our similar image sets
-    // TODO: Use direct references instead of query selector
-    var $similarImageSets = $imageSet.find('[data-similar-image-set]');
-
-    // Filter our similar image sets to selected ones
-    return $similarImageSets.filter(function updateImageSet (i, similarImageSetEl) {
-      return similarImageSetEl.querySelector('[name=save_update]').checked;
-    });
-  }
-
   $('body').on('click', 'button[data-action="accept-similar-images"]', function handleClick (evt) {
-    // Resolve our similar image sets
-    var $selectedSimilarImageSets = findSelectedSimilarImageSets(evt);
-
-    // Accept our image set based on its selection
-    $selectedSimilarImageSets.each(function updateImageSet (i, similarImageSetEl) {
-      // Move back to jQuery collection
-      var $similarImageSet = $(similarImageSetEl);
-      var similarImageSetId = $similarImageSet.data('similar-image-set');
-
-      // Find our original current image
-      var $originalCurrentImg = $similarImageSet.find('.original-current');
-      assert.strictEqual($originalCurrentImg.length, 1);
-      var originalCurrentImgBase64 = utils.getBase64Content($originalCurrentImg[0]);
-
-      // Run accept function
-      // TODO: Remove results when all loaded
-      var imageSet = GlobalState.fetchImageSetById(similarImageSetId);
-      imageSet.acceptChanges(originalCurrentImgBase64);
-    });
+    // Resolve and accept our selected similar image sets
+    var $selectedSimilarImageSets = SimilarImageResults.findSelectedSimilarImageSets(evt);
+    _.each($selectedSimilarImageSets, SimilarImageResults.acceptSimilarImageSet);
   });
   $('body').on('click', 'button[data-action="update-similar-images"]', function handleClick (evt) {
-    // Resolve our similar image sets
-    var $selectedSimilarImageSets = findSelectedSimilarImageSets(evt);
-
-    // Update our image set based on its selection
-    $selectedSimilarImageSets.each(function updateImageSet (i, similarImageSetEl) {
-      // Move back to jQuery collection
-      var $similarImageSet = $(similarImageSetEl);
-      var similarImageSetId = $similarImageSet.data('similar-image-set');
-
-      // Extract updated base64 content
-      var $updatedRefCanvas = $similarImageSet.find('.updated-ref');
-      assert.strictEqual($updatedRefCanvas.length, 1);
-      var base64Data = $updatedRefCanvas[0].toDataURL('image/png');
-
-      // Run update function
-      // TODO: Remove results when all loaded
-      var imageSet = GlobalState.fetchImageSetById(similarImageSetId);
-      imageSet.acceptChanges(base64Data);
-    });
+    // Resolve and update our selected similar image sets
+    var $selectedSimilarImageSets = SimilarImageResults.findSelectedSimilarImageSets(evt);
+    _.each($selectedSimilarImageSets, SimilarImageResults.updateSimilarImageSet);
   });
 };
 
