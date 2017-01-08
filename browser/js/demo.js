ad1edfe47c2e1dda15b3121fef1e2b653b5d4d9a
Todd Wolfson <todd@twolfson.com>
2017-01-07 16:27:56 -0800

    Moved to leverage Mocha's before/after so we can use more Sinon utilities for better mocking

--- browser/js/demo.js
+++ browser/js/demo.js
@@ -3,6 +3,9 @@
 var sinonUtils = require('../../test/utils/sinon');
 var xhrResponses = require('../../test/test-files/http-responses/xhr');
 
+// Define Mocha hook mocks
+global.before = function (fn) { fn(); };
+global.after = function (fn) { /* Don't run after functions */ };
+
 // Start a Sinon server that approves all images
-var sinonContext = {};
-sinonUtils._mockXHR.call(sinonContext, [xhrResponses.UPDATE_IMAGE_SET_APPROVE]);
+sinonUtils.mockXHR([xhrResponses.UPDATE_IMAGE_SET_APPROVE]);
