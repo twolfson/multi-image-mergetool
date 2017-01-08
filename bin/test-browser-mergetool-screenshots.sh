fa71ab1c7cfab5653b74a4e4ef83baca92f08096
Todd Wolfson <todd@twolfson.com>
2017-01-07 17:57:29 -0800

    Updated screenshot signatures

--- bin/test-browser-mergetool-screenshots.sh
+++ bin/test-browser-mergetool-screenshots.sh
@@ -5,11 +5,14 @@ set -e
 # Enable globstar
 shopt -s globstar
 
+# On keyboard interrupt, update our signature info
+trap 'bin/test-browser-update-screenshots-signature.sh test/browser/expected-screenshots' INT
+
 # Compare our images
 bin/multi-image-mergetool \
   --current-images test/browser/actual-screenshots/**/*.png \
   --ref-images $(echo test/browser/actual-screenshots/**/*.png | sed "s/actual-screenshots/expected-screenshots/g") \
   --diff-images $(echo test/browser/actual-screenshots/**/*.png | sed "s/actual-screenshots/diff-screenshots/g")
 
-# Update our signature info
+# If we had no errors, update screenshot signature
 bin/test-browser-update-screenshots-signature.sh test/browser/expected-screenshots
